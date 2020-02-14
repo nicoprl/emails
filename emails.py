132c894611a220be297a7111f46ffa348fbbd05c
nicoprl <nicolas.perrel@gmail.com>
2020-02-14 23:15:59 +0100

    -config creates config.json file

--- emails.py
+++ emails.py
@@ -6,6 +6,7 @@ import smtplib
 import json
 import argparse
 import ntpath
+from getpass import getpass
 from email.mime.multipart import MIMEMultipart
 from email.mime.text import MIMEText
 from email.mime.base import MIMEBase
@@ -14,44 +15,51 @@ from email import encoders
 def main():
     description = """
     Send email(s). Add additionnal -file tags to send several attachments.
-    -user, -pwd, -smtp and -port flags are optionnal if --config CONFIG is provided
-    config file must be a json file containing: user, password, smtp, port"""
+    -user, -pwd, -smtp and -port flags are optionnal if a config file exists"""
     parser = argparse.ArgumentParser(description=description)
-    parser.add_argument('-user', required=False, help='username@domain.com')
-    parser.add_argument('-pwd', required=False, help='password')
-    parser.add_argument('-smtp', required=False, help='SMTP')
-    parser.add_argument('-port', required=False, help='port')
-    parser.add_argument('-to', required=True, metavar='adr1;adr2 ...', help='recipient adresses')
-    parser.add_argument('-subject', required=True, help='subject')
-    parser.add_argument('-body', required=False, help='body')
-    parser.add_argument('--config', required=False, help='JSON config file containing auth infos')
+    parser.add_argument('-user', help='username@domain.com')
+    parser.add_argument('-pwd', help='password')
+    parser.add_argument('-smtp', help='SMTP')
+    parser.add_argument('-port', help='port')
+    parser.add_argument('-to', metavar='adr1;adr2 ...', help='recipient adresses')
+    parser.add_argument('-subject', help='subject')
+    parser.add_argument('-body', help='body')
+    parser.add_argument('-config', action="store_true", help='create JSON config file containing auth infos')
     parser.add_argument('-file', action='append', metavar='PATH', help='attachment')
     args = parser.parse_args()
 
-    if args.config is not None:
-        user, pwd, smtp, port = get_auth_from_file(args.config)
-    elif (args.user and args.pwd and args.smtp and args.port) is not None:
-        user = args.user
-        pwd = args.pwd
-        smtp = args.smtp
-        port = args.port
-    else:
-        print('ERROR: provide -user, -pwd, -smtp and -port if --config is missing')
-        sys.exit(1)
+    if len(sys.argv) < 2:
+        parser.print_help()
 
-    if args.body is None:
-        body = ''
-    else:
-        body = args.body
-    
-    for recipient in args.to.split(';'):
-        try:
-            sendEmail(user, pwd, smtp, port, recipient, args.subject, body, args.file)
-        except Exception as e:
-            print(e)
-            sys.exit(1)
+    if args.config:
+        config()
+
+    if (args.to is not None) and (args.subject is not None):
+        if (args.user and args.pwd and args.smtp and args.port) is not None:
+            user = args.user
+            pwd = args.pwd
+            smtp = args.smtp
+            port = args.port
         else:
-            print("done.")
+            try:
+                user, pwd, smtp, port = get_auth_from_file(sys.path[0] + "/config.json")
+            except:
+                print('ERROR: provide -user, -pwd, -smtp and -port if --config is missing')
+                sys.exit(1)
+
+        if args.body is None:
+            body = ''
+        else:
+            body = args.body
+        
+        for recipient in args.to.split(';'):
+            try:
+                sendEmail(user, pwd, smtp, port, recipient, args.subject, body, args.file)
+            except Exception as e:
+                print(e)
+                sys.exit(1)
+            else:
+                print("done.")
 
 def sendEmail(user, pwd, smtp, port, to, subject, text, attachment=[]):
     msg = MIMEMultipart()
@@ -90,6 +98,21 @@ def get_auth_from_file(jsonFile):
     except Exception as e:
         print(e)
         sys.exit(1)
+
+def config():
+    user = input("email address: ")
+    password = getpass("password: ")
+    smtp = input("SMTP: ")
+    port = input("port: ")
+
+    with open(sys.path[0] + "/config.json", mode="w", encoding="utf-8") as conf:
+        conf.write(json.dumps({
+            "user": user,
+            "password": password,
+            "smtp": smtp,
+            "port": port 
+        }))
+    print(sys.path[0] + "/config.json")
 
 if __name__ == '__main__':
     main()
